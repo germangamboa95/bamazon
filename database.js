5b5ccf75d01627ca29f056a2de47ff0bf8a7c5e1
German Gamboa <germangamboa95@gmail.com>
2018-04-30 23:09:09 -0400

    still working on heroku

--- database.js
+++ database.js
@@ -1,15 +1,14 @@
 var mysql = require("mysql");
 
 module.exports =  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+    host: "us-cdbr-iron-east-05.cleardb.net",
 
     // Your username
-    user: "root",
+    user: "b1b33428f65f9b",
 
     // Your password
-    password: "",
-    database: "bamazon"
+    password: "3ea11123",
+    database: "heroku_0420777eb6f6d22"
     });
 
 
